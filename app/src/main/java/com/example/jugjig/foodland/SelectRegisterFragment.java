3127bd91e4cb5230f24895461eca6eddb49eb182
Suttida Sat <59070178@kmitl.ac.th>
2018-10-31 01:08:33 +0700

    UI เลือกลงทะเบียน + รีจิส
    ยังรีจิสไม่ได้ ยังไม่ได้ทำ

--- app/src/main/java/com/example/jugjig/foodland/SelectRegisterFragment.java
+++ app/src/main/java/com/example/jugjig/foodland/SelectRegisterFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
@@ -30,6 +33,22 @@ public class SelectRegisterFragment extends Fragment {
     }
 
     void selectCustomer(){
+
+        Button cus = getView().findViewById(R.id.customer_regis);
+        cus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.main_view,new RegisterCustomerFragment())
+                        .commit();
+                Log.d("USER", "GOTO Register");
+                Toast.makeText
+                        (getContext(),"GO TO Register",Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
 
     }
 
