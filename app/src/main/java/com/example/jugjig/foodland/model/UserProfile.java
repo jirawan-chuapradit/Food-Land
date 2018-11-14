36d6f2aa21ba90073888f3e15279044da63df230
Nopparut <ak3.nopparut@gmail.com>
2018-11-14 17:10:09 +0700

    update SQL user, history in login and logout

--- app/src/main/java/com/example/jugjig/foodland/model/UserProfile.java
+++ app/src/main/java/com/example/jugjig/foodland/model/UserProfile.java
@@ -1,5 +1,7 @@
 package com.example.jugjig.foodland.model;
 
+import android.content.ContentValues;
+
 public class UserProfile {
 
     /**************************************
@@ -9,16 +11,38 @@ public class UserProfile {
 
     private static UserProfile userProfileInstance;
 
-   public static UserProfile getRestProfileInstance(){
-       if (userProfileInstance == null){
-           userProfileInstance = new UserProfile();
-       }
-       return userProfileInstance;
-   }
+    private ContentValues contentValues = new ContentValues();
+
+    public static UserProfile getRestProfileInstance() {
+        if (userProfileInstance == null) {
+            userProfileInstance = new UserProfile();
+        }
+        return userProfileInstance;
+    }
 
     private UserProfile() {
     }
 
+    public UserProfile(String fName, String lName, String phone, String email, String desc, String role) {
+        this.fName = fName;
+        this.lName = lName;
+        this.phone = phone;
+        this.email = email;
+        this.desc = desc;
+        this.role = role;
+    }
+
+    public void setContentValues(String userId){
+        this.contentValues.put("userId", userId);
+        this.contentValues.put("firstname", fName);
+        this.contentValues.put("lastname", lName);
+        this.contentValues.put("email", email);
+        this.contentValues.put("phone", phone);
+    }
+
+    public ContentValues getContentValues() {
+        return this.contentValues;
+    }
 
     public String getRole() {
         return role;
